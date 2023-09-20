759df3e4ca66d29da6cf6e416ef00c44ec6428e1
KXRXZ <126346482+KXRXZ@users.noreply.github.com>
2023-09-20 18:06:28 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MAUIApp2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFirstImageClicked(object sender, EventArgs e)
+        { 
+                 imageButton1.Source = "reality1_img.png";
+            }
+        private void OnSecondImageClicked(object sender, EventArgs e)
+        {
+                imageButton2.Source = "reality2_img.png";
+            }
+        }
+    }
